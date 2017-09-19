2423e9482d501277062908b91a082af63814931f
hamartia0 <zbylonely@gmail.com>
2017-09-19 20:16:52 +0800

    利用Runtime进行归档解档

--- EncodeWithRuntime/EncodeWithRuntime/MyTestObject.m
+++ EncodeWithRuntime/EncodeWithRuntime/MyTestObject.m
@@ -7,20 +7,53 @@
 //
 
 #import "MyTestObject.h"
+#import <objc/message.h>
 
 @implementation MyTestObject
 
 - (void)encodeWithCoder:(NSCoder *)aCoder {
-    [aCoder encodeObject:_officalName forKey:@"officalName"];
-    [aCoder encodeInt:_amountOfNames forKey:@"amountOfNames"];
-    [aCoder encodeObject:_aliasArr forKey:@"aliasArr"];
+//    [aCoder encodeObject:_officalName forKey:@"officalName"];
+//    [aCoder encodeInt:_amountOfNames forKey:@"amountOfNames"];
+//    [aCoder encodeObject:_aliasArr forKey:@"aliasArr"];
+    
+    unsigned int ivarCount = 0;
+    
+    Ivar *ivarList = class_copyIvarList([MyTestObject class], &ivarCount);
+    
+    for (int i = 0; i < ivarCount; i++) {
+        Ivar ivar = ivarList[i];
+        const char *ivarName = ivar_getName(ivar);
+        NSString *ivarStr = [NSString stringWithUTF8String:ivarName];
+        
+        id value = [self valueForKey:ivarStr];
+        [aCoder encodeObject:value forKey:ivarStr];
+    }
+    
+    free(ivarList);
+    
 }
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     if (self = [super init]) {
-        self.officalName = [aDecoder decodeObjectForKey:@"officalName"];
-        self.amountOfNames = [aDecoder decodeIntForKey:@"amountOfNames"];
-        self.aliasArr = [aDecoder decodeObjectForKey:@"aliasArr"];
+//        self.officalName = [aDecoder decodeObjectForKey:@"officalName"];
+//        self.amountOfNames = [aDecoder decodeIntForKey:@"amountOfNames"];
+//        self.aliasArr = [aDecoder decodeObjectForKey:@"aliasArr"];
+        
+        unsigned int ivarCount = 0;
+        
+        Ivar *ivarList = class_copyIvarList([MyTestObject class], &ivarCount);
+        
+        for (int i = 0; i < ivarCount; i++) {
+            Ivar ivar = ivarList[i];
+            const char *ivarName = ivar_getName(ivar);
+            NSString *ivarStr = [NSString stringWithUTF8String:ivarName];
+            
+//            id value = [self valueForKey:ivarStr];
+            [self setValue:[aDecoder decodeObjectForKey:ivarStr] forKey:ivarStr];
+            
+        }
+        
+        free(ivarList);
     }
     return self;
 }
